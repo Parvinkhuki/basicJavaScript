2aae8a6f2cf95b2282fae831f86b426331ce354e
Parvin Khuki <138358501+Parvinkhuki@users.noreply.github.com>
2024-02-07 20:07:30 +0600

    prb04

---
+++ prb04.js
@@ -0,0 +1,6 @@
+// Sarah's mother is teaching her mathematics. She gave Sarah a number and asked her what the remainder would be if she deivided the number by 5. help sarah write the program.......
+// input: 119
+
+let number = 119;
+ let remainder = 119%5
+ console.log(remainder)
