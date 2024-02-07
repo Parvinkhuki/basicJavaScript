4ea2672adb5009ed57403cb4497775106ccdc4bc
Parvin Khuki <138358501+Parvinkhuki@users.noreply.github.com>
2024-02-07 18:55:50 +0600

    first commit

---
+++ prb01.js
@@ -0,0 +1,18 @@
+
+
+//.......... Harry's mom gave him money and asked him to buy some oranges and apples . write a program to help Harry calculate how much money the shopokeeper will return...............
+
+
+
+
+let harryMomGave =1000;
+let oranges=300
+let apples = 400;
+
+shopkeeperWillReturn =1000-(300+400)
+
+console.log(shopkeeperWillReturn)
+
+
+
+
