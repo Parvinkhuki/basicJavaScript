797b33c41a5eab30bf40f3ff28577e8876a9ac59
Parvin Khuki <138358501+Parvinkhuki@users.noreply.github.com>
2024-02-07 19:08:35 +0600

    prb2

---
+++ prb02.js
@@ -0,0 +1,15 @@
+// Write a program to calculate the acerage marks of Mathematics biology Chemistry Physics,and Bangla of student.
+
+
+// SamPle : 75.25, 65, 80, 35.45 ,99.50
+
+let mathematics = 75.25;
+let chemistry = 65;
+let physics = 80;
+let biology = 35.45;
+let bangla= 99.50;
+
+let average =((mathematics + chemistry + physics + biology + bangla)/5).toFixed(2)
+
+console.log(average)
+
