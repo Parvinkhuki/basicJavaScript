d1f9f6d92e20a9c8d1be95423de62bff55a091b5
Parvin Khuki <138358501+Parvinkhuki@users.noreply.github.com>
2024-02-07 20:01:37 +0600

    prb03

---
+++ prb03.js
@@ -0,0 +1,8 @@
+// John's teacher gave him two variables.Each variable contains a string. John's teacher asked him to combinw these two steings and print them in one line help john write the program.
+
+
+let firstLine ='i am going to be'
+let secondLine = 'an awesome web developer'
+
+let oneLine = firstLine + ' ' + secondLine
+console.log(oneLine)
