30c1f3b221c277547392a4864f44f08e0d40cff9
Parvin Khuki <138358501+Parvinkhuki@users.noreply.github.com>
2024-02-11 20:01:34 +0600

    prob05

---
+++ prb05.js
@@ -0,0 +1,20 @@
+/* You are given an array:
+var fruits =['Apple', 'Banana', 'Orange']
+A) Find the index of 'Banana' and replace 'Banana' with 'Mango'
+B) Remove 'Orange' and add 'Watermelon'
+*/
+
+
+
+// PRoblem 01 
+var fruits =['Apple', 'Banana', 'Orange']
+console.log(fruits)
+var indexOfBanana = fruits.indexOf("Banana")
+
+console.log(indexOfBanana)
+
+// problem 02
+
+ fruits[2] ='Watermelon'
+
+ console.log(fruits)
